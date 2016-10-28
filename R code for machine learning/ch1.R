2ba1d4618d1e96fe2bd3d731241f7a090adb5cf9
eric <eric_yang09@126.com>
2016-10-28 23:31:05 +0800

    一个逗比的日常#

--- R code for machine learning/ch1.R
+++ R code for machine learning/ch1.R
@@ -12,23 +12,21 @@ ggplot(data,aes(Population,Profit))+geom_point(colour="red")
 # 一个维度下的回归
 
 costfunc1 <- function(data, alpha=0.01, theta = c(0,0),iteration = 500) {
-  
-   # 学习率设置为0, 初始值设置为c(2,1)
+  # 如果把iteration设置为0，就是一个单纯计算的公式
+  # 学习率设置为0, 初始值设置为c(2,1)
   n <- nrow(data)
   X <- cbind(rep(1,n), data[,1]) # 设计矩阵
-  theta <- as.matrix(theta)
-  
+
   df <- data.frame(theta0=rep(0,iteration+1),
                    theta1 = rep(0, iteration + 1),
                    cost = rep(0,iteration + 1))
   
-  #function(theta=c(2,1)){
   lapply(1:(iteration+1), FUN = function(i){
     yhat <- X %*% theta   #拟合值
-    cost <- (yhat - data[,2])^2/(2*n)
-    df[i,] <<- c(theta[1], theta[2], cost)
+    cost <- sum((yhat - data[,2])^2)/(2*n)
+    df[i,] <<- c(theta, cost)
     theta <<- theta - t(X) %*% (yhat-data[,2]) * alpha / n
-  
+    
   })
   
   return(df)
@@ -38,4 +36,35 @@ test <- costfunc1(data,alpha = 0.01, theta = c(0,0), iteration = 1000)
 tail(test)
 plot(test$cost[-1],col="red",type="l")
 
-contour(x=test$theta0,y=test$theta1,z=test$cost)
+## 考虑二元线性回归
+path2 <- "E:/python学习/吴恩达机器学习/machine-learning-ex1/ex1/ex1data2.txt"
+data2 <- read.csv(path2, header = F)
+data2 <- scale(data2, center = TRUE, scale = TRUE)
+costfunc2 <- function(data, p, alpha=0.01, theta, iteration = 500) {
+  # p表示自变量个数
+  # 如果把iteration设置为0，就是一个单纯计算的公式
+  
+  n <- nrow(data)
+  #theta <- rep(0,p+1)
+  X <- as.matrix(cbind(rep(1,n), data[,1:p])) # 设计矩阵
+
+  df <- data.frame(theta0=rep(0,iteration+1),
+                   theta1 = rep(0, iteration + 1),
+                   theta2 = rep(0, iteration + 1),
+                   cost = rep(0,iteration + 1))
+
+  lapply(1:(iteration+1), FUN = function(i){
+    yhat <- X %*% theta   #拟合值
+    cost <- sum((yhat - data[,p+1])^2)/(2*n)
+    df[i,] <<- c(theta, cost)
+    theta <<- theta - t(X) %*% (yhat-data[,p+1]) * alpha / n
+    
+  })
+  
+  return(df)
+}
+
+
+test2 <- costfunc2(data2, p=2, alpha = 0.01, theta = c(0,0,0), iteration = 1000)
+
+
