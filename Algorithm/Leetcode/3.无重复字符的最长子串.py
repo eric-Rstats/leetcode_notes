9aaf5eea8089fe14aa4c8bf11c4e1364590b044e
eric-Rstats <eric_yang09@126.com>
2023-03-09 15:52:06 +0000

    窗口

--- Algorithm/Leetcode/3.无重复字符的最长子串.py
+++ Algorithm/Leetcode/3.无重复字符的最长子串.py
@@ -7,40 +7,35 @@
 # @lc code=start
 
 
-""" class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        # 定义出现的字符集合
-        char_set = {}
-        max_size = 0
-        start = 0
-        for i in range(len(s)):
-            if s[i] in char_set:
-                start = max(char_set[s[i]], start)
-            max_size = max(max_size, i-start+1)
-            char_set[s[i]] = i+1
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         # 定义出现的字符集合
+#         char_set = {}
+#         max_size = 0
+#         start = 0
+#         for i, item in enumerate(s):
+#             if item in char_set:
+#                 start = max(char_set[item], start)
+#             max_size = max(max_size, i-start+1)
+#             char_set[item] = i+1
 
-        return max_size """
+#         return max_size
 
 # @lc code=en
 
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left, right = 0, 0
         result = 0
-        needs = dict(Counter(s))
-        window = {item: 0 for item in needs}
-
-        while right < len(s):
-            c1 = s[right]
-            window[c1] += 1
-
-            while window[c1] > 1:
-                c2 = s[left]
-                window[c2] -= 1
+        left = 0
+        char_map = {}
+        for right in range(len(s)):
+            while s[right] in char_map:
+                char_map[s[left]] -= 1
+                if char_map[s[left]] == 0:
+                    del char_map[s[left]]
                 left += 1
+            char_map[s[right]] = 1
 
             result = max(result, right-left+1)
-
-            right += 1
         return result
---
+++ Algorithm/Leetcode/567.字符串的排列.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=567 lang=python3
+#
+# [567] 字符串的排列
+#
+
+# @lc code=start
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        needs = {}
+        for item in s1:
+             needs[item] = needs.get(item, 0) + 1
+        n, m = len(s1), len(s2)
+        window = {}
+        left = 0
+        for right in range(m):
+            item = s2[right]
+            window[item] = window.get(item, 0) + 1
+            if window == needs:
+                return True
+            if right >= n - 1:
+                window[s2[left]] -= 1
+                if window[s2[left]] == 0:
+                    del window[s2[left]]
+                left += 1
+
+        return False
+            
+# @lc code=end
+
