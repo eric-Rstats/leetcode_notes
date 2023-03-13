ede2ea3e596226195b4333664d7262e5d2ef175d
eric-Rstats <eric_yang09@126.com>
2023-03-13 05:27:14 +0000

    904-水果成篮

--- Algorithm/Leetcode/904.水果成篮.py
+++ Algorithm/Leetcode/904.水果成篮.py
@@ -7,5 +7,18 @@
 # @lc code=start
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        left, result = 0, 0
+        cnt = {}
+        for right in range(len(fruits)):
+            tail = fruits[right]
+            cnt[tail] = cnt.get(tail, 0) + 1
+            while len(cnt) > 2:
+                head = fruits[left]
+                cnt[head] -= 1
+                if cnt[head] == 0:
+                    del cnt[head]
+                left += 1
+            result = max(right-left+1, result)
+        return result
 # @lc code=end
 
