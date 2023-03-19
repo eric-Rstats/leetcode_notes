8f1a7fdd0b0698ba3960f749ed5fea8307bdb9b0
eric-Rstats <eric_yang09@126.com>
2023-03-19 13:39:21 +0000

    二叉树

---
+++ Algorithm/Leetcode/144.二叉树的前序遍历.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=144 lang=python3
+#
+# [144] 二叉树的前序遍历
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def travel(root):
+            if not root: return
+            result.append(root.val)
+            travel(root.left)
+            travel(root.right)
+        
+        travel(root)
+        return result
+    # @lc code=end
+
---
+++ Algorithm/Leetcode/94.二叉树的中序遍历.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=94 lang=python3
+#
+# [94] 二叉树的中序遍历
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def travel(root):
+            if not root: return
+            travel(root.left)
+            result.append(root.val)
+            travel(root.right)
+        
+        travel(root)
+        return result
+# @lc code=end
+
